775cb9ba5bfa033f61f1a2b42b37bd83a895325f
ChenYF <CCCChenYF@gmail.com>
2022-01-25 10:39:45 +0800

    add another comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 //some comment by chenyf
 class HelloWorld {
+	//the main method is necessary
 	public static void main(String[] args) {
 		//output the message
 		System.out.println("Hello World");
